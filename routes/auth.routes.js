6c950dbecb8d0001c3f9b5010cb3e6f4cc15802b
Stephen Muñez <sjtmunez@gmail.com>
2025-02-10 12:20:11 +0800

    feat: authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
+import { signIn, signOut, signUp } from '../controllers/auth.controller.js';
 
-const authRouter = Router();
+const authRouter = new Router();
 
-authRouter.post('/sign-up', (req, res) => res.send({ message: 'Sign up' }));
-
-authRouter.post('/sign-in', (req, res) => res.send({ message: 'Sign in' }));
-
-authRouter.post('/sign-out', (req, res) => res.send({ message: 'Sign out' }));
+authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-in', signIn);
+authRouter.post('/sign-out', signOut);
 
 export default authRouter;
