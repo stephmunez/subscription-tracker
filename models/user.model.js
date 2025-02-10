e05397ae2e151b394f0e118d01b520484a506e9a
Stephen Muñez <sjtmunez@gmail.com>
2025-02-10 10:12:51 +0800

    feat: models

---
+++ models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'User name is required'],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, 'User email is required'],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      minLength: 5,
+      maxLength: 255,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        'Please enter a valid email address',
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, 'User password is required'],
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
