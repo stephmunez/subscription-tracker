a3c44f21dd31672e0c025c36da0f04685b9852a8
Stephen Muñez <sjtmunez@gmail.com>
2025-02-11 10:35:23 +0800

    feat: arcjet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import { PORT } from './config/env.js';
 
 import connectToDatabase from './database/mongodb.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
@@ -14,6 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth/', authRouter);
 app.use('/api/v1/users/', userRouter);
