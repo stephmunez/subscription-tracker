71b7bb74c2d78da946637eeeb7d5e07a90c16d4a
Stephen Muñez <sjtmunez@gmail.com>
2025-02-12 12:11:47 +0800

    refactor: auto-update renewal date

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -1,5 +1,7 @@
 import dayjs from 'dayjs';
 import { createRequire } from 'module';
+import { SERVER_URL } from '../config/env.js';
+import { workflowClient } from '../config/upstash.js';
 import Subscription from '../models/subscription.model.js';
 const require = createRequire(import.meta.url);
 const { serve } = require('@upstash/workflow/express');
@@ -27,21 +29,6 @@ export const sendReminders = serve(async (context) => {
     return;
   }
 
-  if (dayjs().isSame(renewalDate, 'day')) {
-    const period = RENEWAL_PERIODS[subscription.frequency];
-    renewalDate = renewalDate.add(period, 'day');
-
-    await Subscription.findByIdAndUpdate(subscriptionId, {
-      renewalDate: renewalDate.toDate(),
-    });
-
-    console.log(
-      `Renewal date updated for subscription ${subscriptionId} to ${renewalDate.format(
-        'YYYY-MM-DD'
-      )}`
-    );
-  }
-
   for (const daysBefore of REMINDERS) {
     const reminderDate = renewalDate.subtract(daysBefore, 'day');
 
@@ -55,6 +42,39 @@ export const sendReminders = serve(async (context) => {
 
     await triggerReminder(context, `Reminder ${daysBefore} days before`);
   }
+
+  if (!renewalDate.isSame(dayjs())) {
+    console.log(
+      `Sleeping until renewal date for subscription ${subscriptionId}`
+    );
+    await sleepUntilReminder(context, 'Renewal Day', renewalDate);
+  }
+
+  const period = RENEWAL_PERIODS[subscription.frequency];
+  renewalDate = renewalDate.add(period, 'day');
+
+  await Subscription.findByIdAndUpdate(subscription._id, {
+    renewalDate: renewalDate.toISOString(),
+  });
+
+  console.log(
+    `Renewal date updated for subscription ${subscriptionId} to ${renewalDate.format(
+      'YYYY-MM-DD'
+    )}`
+  );
+
+  await workflowClient.trigger({
+    url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+    body: {
+      subscriptionId,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    retries: 0,
+  });
+
+  console.log(`Workflow re-triggered for subscription ${subscriptionId}`);
 });
 
 const fetchSubscription = async (context, subscriptionId) => {
